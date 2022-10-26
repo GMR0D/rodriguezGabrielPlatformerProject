7f9748bfc5c577d1c022fd6129c8228fb7ea0935
GMR0D <gmrodriguez@mail.bradley.edu>
2022-10-26 16:29:54 -0700

    10/26/2022 Update

---
+++ Assets/Code/Level Menus/Sanctum.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sanctum : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            SceneManager.LoadScene(8);
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
