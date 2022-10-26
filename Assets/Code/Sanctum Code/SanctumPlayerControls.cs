7471b6e4defdcbdecad6f8b69a96a8aa5b1e7ad8
GMR0D <gmrodriguez@mail.bradley.edu>
2022-10-26 16:47:52 -0700

    10/26/2022 Update 2

---
+++ Assets/Code/Sanctum Code/SanctumPlayerControls.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SanctumPlayerControls : MonoBehaviour
+{
+    public float Speed;
+    public float Health = 1;
+    public GameObject Player;
+    public Rigidbody2D Rigidbody;
+    public float Jump;
+    public Rigidbody2D Deathwall;
+    public EnemyBehavior EBehavior;
+    public GameObject ExitPortal;
+
+    void Start()
+    {
+        Vector3 SpawnPosition = new Vector3(-6.9f, -4.42f, 0f);
+        transform.position = SpawnPosition;
+    }
+
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+
+        Vector2 newPosition = gameObject.transform.position;
+
+        newPosition.x += xMove * Speed * Time.deltaTime;
+
+        transform.position = newPosition;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody.AddForce(Vector2.up * Jump, ForceMode2D.Impulse);
+            Jump = 0;
+            Invoke("JumpReset", 1.2f);
+        }
+        if (Input.GetKeyUp(KeyCode.A) || (Input.GetKeyUp(KeyCode.LeftArrow)))
+        {
+            Speed = 0.25f;
+        }
+        if (Input.GetKeyUp(KeyCode.D) || (Input.GetKeyUp(KeyCode.RightArrow)))
+        {
+            Speed = 0.25f;
+        }
+        if (Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+            Speed = 5;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || (Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            Speed = 5;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "CrystalSpike")
+        {
+            SceneManager.LoadScene(8);
+        }
+        if (collision.gameObject.tag == "Deathwall")
+        {
+            SceneManager.LoadScene(8);
+        }
+        if (collision.gameObject.tag == "EnemySpell")
+        {
+            SceneManager.LoadScene(8);
+        }
+        if (collision.gameObject.tag == "Hexer")
+        {
+            SceneManager.LoadScene(8);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "SpawnerActivation")
+        {
+            EBehavior.EnemySpawn();
+        }
+        if (collision.gameObject.tag == "ExitPortal")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+    void JumpReset()
+    {
+        Jump = 8;
+    }
+}
